7aa27ec1afc1b40228ff70b7823b938e5153d7b0
Diego Gomes <diego.gomes.191287@gmail.com>
2022-05-25 12:55:47 -0300

    first commit

---
+++ TesteLog4net/Person.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using log4net;
+
+namespace TesteLog4net
+{
+    class Person
+    {
+        // create a static logger field
+        //private static ILog logger = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        public string Name { get; set; }
+        public string LastName { get; set; }
+
+        public string FirstName { get; set; }
+        public DateTime LastDate { get; set; }
+
+        public int Id { get; set; }
+
+        //public Person(string name, string lastName)
+        //{
+        //    Name = name;
+        //    LastName = lastName;
+
+        //    logger.Info(String.Format("Created a person {0} at {1}", this, DateTime.Now));
+        //}
+
+        public override string ToString()
+        {
+            return String.Format("[{0} {1}]", Name, LastName);
+        }
+    }
+}
+
---
+++ TesteLog4net/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+[assembly: log4net.Config.XmlConfigurator(ConfigFile = "log4net.config")]
+
+namespace TesteLog4net
+{
+    internal class Program
+    {
+        private static readonly log4net.ILog logger = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+        static void Main(string[] args)
+        {
+
+            // create 2 persons
+            var person1 = new Person();
+            person1.Name = "Diego";
+            person1.LastName = "Gomes";
+            //var person1 = new Person("Jonh", "Gold");
+            //var person2 = new Person("James", "Miller");
+            // create 2 cars
+            var car1 = new Car("Tesla Model S", 2020, person1);
+            //var car2 = new Car("Tesla Model X", 2020, person2);
+            // logging
+            logger.Debug("Some debug log");
+            logger.Info(String.Format("{0} {1}", person1.Name, person1.LastName));
+            //logger.Info(String.Format("Car2: {0}", car2));
+            logger.Warn(String.Format("Warning accrued at {0}", DateTime.Now));
+            logger.Error(String.Format("Error accrued at {0}", DateTime.Now));
+            logger.Fatal(String.Format("Serious problem with car {0} accrued at {1}", car1, DateTime.Now));
+        }
+    }
+}
